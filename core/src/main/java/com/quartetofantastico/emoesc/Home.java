6ef1b65bc233c2907ca0cc4d0a4bdb519dbbc0b2
albert-r0ss <a.a.a.666kkj@gmail.com>
2026-05-24 07:35:16 +0100

    Adicionei MenuScreen com botões e modal de créditos

--- core/src/main/java/com/quartetofantastico/emoesc/Home.java
+++ core/src/main/java/com/quartetofantastico/emoesc/Home.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.quartetofantastico.emoesc.interfaceUser.GameScreen;
+import com.quartetofantastico.emoesc.interfaceUser.OpScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Home extends Game{
@@ -18,8 +19,7 @@ public class Home extends Game{
        //batch = new SpriteBatch();
         //font=new BitmapFont();
         //image = new Texture("libgdx.png");
-        setScreen(new GameScreen());
-        setScreen(new GameScreen());
+        setScreen(new OpScreen(this));
     }
 
     @Override
