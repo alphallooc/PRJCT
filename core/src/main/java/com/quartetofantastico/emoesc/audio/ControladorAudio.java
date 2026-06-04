3cee55eed5f3d557c7ba7b9946f54367ef24ce9b
ManuelDaSilvaEsteves <manuelesteves778@gmail.com>
2026-06-04 09:50:45 +0100

    classe Som, Class ControladorAudio

--- core/src/main/java/com/quartetofantastico/emoesc/audio/ControladorAudio.java
+++ core/src/main/java/com/quartetofantastico/emoesc/audio/ControladorAudio.java
@@ -2,113 +2,59 @@ package com.quartetofantastico.emoesc.audio;
 
 import com.badlogic.gdx.audio.Music;
 
-class ControladorAudio {
+public class ControladorAudio {
     private static Music musicaAtual;
-    private static String entidadeAtual = "Medico";
+    private static String entidadeAtual = "policial";
 
-    public static void iniciar() {
+    public static void iniciar(){
         Sons.carregar();
-        Sons.START.play(); // som ao iniciar jogo
-    }
-
-    public static void escolherEntidade(String entidade) {
-        entidadeAtual = entidade.toLowerCase();
         Sons.START.play();
-        trocarMusica(1); // começa a Fase1 = Dia
     }
-
-    //Regra: FASE1=DIA, FASE2=TARDE, FASE3=NOITE
-    public static void trocarMusica(int fase) {
+    public static void escolherEntidade(String entidade){
+        entidadeAtual = entidade.toLowerCase();
+        trocarMusica(1);
+    }
+    public static void trocarMusica(int fase){
         if (musicaAtual != null) musicaAtual.stop();
-
-        if (entidadeAtual.equals("medico")) {
-            if (fase == 1) musicaAtual = Sons.MEDICO_FASE1;
-            if (fase == 2) musicaAtual = Sons.MEDICO_FASE2;
-            if (fase == 3) musicaAtual = Sons.MEDICO_FASE3;
-        }
-        if (entidadeAtual.equals("policial")) {
-            if (fase == 1) musicaAtual = Sons.POLICIAL_FASE1;
-            if (fase == 2) musicaAtual = Sons.POLICIAL_FASE2;
-            if (fase == 3) musicaAtual = Sons.POLICIAL_FASE3;
-        }
-        if (entidadeAtual.equals("ninja")) {
-            if (fase == 1) musicaAtual = Sons.NINJA_FASE1;
-            if (fase == 2) musicaAtual = Sons.NINJA_FASE2;
-            if (fase == 3) musicaAtual = Sons.NINJA_FASE3;
-        }
-        if (entidadeAtual.equals("bombeiro")) {
-            if (fase == 1) musicaAtual = Sons.BOMBEIRO_FASE1;
-            if (fase == 2) musicaAtual = Sons.BOMBEIRO_FASE2;
-            if (fase == 3) musicaAtual = Sons.BOMBEIRO_FASE3;
-        }
-        if (entidadeAtual.equals("estudante")) {
-            if (fase == 1) musicaAtual = Sons.ESTUDANTE_FASE1;
-            if (fase == 2) musicaAtual = Sons.ESTUDANTE_FASE2;
-            if (fase == 3) musicaAtual = Sons.ESTUDANTE_FASE3;
-        }
-        if (musicaAtual != null) musicaAtual.play();
+        if (fase == 1) musicaAtual = Sons.DIA;
+        if (fase == 2) musicaAtual = Sons.TARDE;
+        if (fase == 1) musicaAtual = Sons.NOITE;
+        musicaAtual.play();
     }
-
-    //Regra: cada entidade tem som unico ao pegar emoji
-    public static void tocarSomEmoji() {
+    public static void tocarSomemoji(){
         if (entidadeAtual.equals("ninja")) Sons.NINJA.play();
-        if (entidadeAtual.equals("medico")) Sons.MEDICO.play();
         if (entidadeAtual.equals("policial")) Sons.POLICIAL.play();
-        if (entidadeAtual.equals("bombeiro")) Sons.BOMBEIRO.play();
         if (entidadeAtual.equals("estudante")) Sons.ESTUDANTE.play();
     }
 
-    //Regra: emoji errado = som erro + hostil spawna
-    public static void emojiErro() {
+    public static void emojiErrado(){
         Sons.ERRO.play();
         Sons.HOSTIL.play();
     }
-
-    //Regra: porta abre no fim da fase
-    public static void abrirPorta() {
+    public static void abrirPorta(){
         Sons.PORTA.play();
     }
-
-    public static void tocarTranporte() {
+    public static void tocarTransporte(){
         if (entidadeAtual.equals("ninja")) Sons.NINJA_TRANS.play();
-        if (entidadeAtual.equals("medico")) Sons.MEDICO_TRANS.play();
         if (entidadeAtual.equals("policial")) Sons.POLICIAL_TRANS.play();
-        if (entidadeAtual.equals("bombeiro")) Sons.BOMBEIRO_TRANS.play();
-        if (entidadeAtual.equals("estudante")) Sons.Estudante_Trans.play();
+        if (entidadeAtual.equals("estudante")) Sons.ESTUDANTE_Trans.play();
     }
-
-    // Regras: beep nos ultimos 10s
-    public static void timerBeep() {
+    public static void timerBeep(){
         Sons.TIMER_BEEP.play();
     }
-
-    //Regra 4 tipos de animais
-    public static void animalAmigavel() {
-        Sons.ANIMAL_AMIGAVEL.play();
-    }
-
-    public static void animalNeutro() {
-        Sons.ANIMAL_NEUTRO.play();
-    }
-
-    public static void animalRestrito() {
+    public static void animalRestrito(){
         Sons.ANIMAL_RESTRITO.play();
     }
-
-    public static void hostilAtaca() {
+    public static void hostilAtaca(){
         Sons.HOSTIL.play();
-        Sons.VIDA_PERDEU.play();
+        Sons.VIDA_PERDIDA.play();
     }
-
-    public static void gameOver() {
+    public static void gameOver(){
         if (musicaAtual != null) musicaAtual.stop();
         Sons.GAMEOVER.play();
     }
-
-    public static void parar() {
+    public static void para(){
         if (musicaAtual != null) musicaAtual.stop();
         Sons.dispose();
     }
-
-
 }
--- core/src/main/java/com/quartetofantastico/emoesc/audio/Sons.java
+++ core/src/main/java/com/quartetofantastico/emoesc/audio/Sons.java
@@ -5,88 +5,59 @@ import com.badlogic.gdx.audio.Sound;
 public class Sons {
 
     // Sons gerais do jogo
-    public static Sound START, PORTA, ERRO, VIDA_PERDEU, GAMEOVER, TIMER_BEEP;
+    public static Sound START, PORTA, ERRO, VIDA_PERDIDA, GAMEOVER, TIMER_BEEP, TIMER;
 
     // 4 tipos de animais
-    public static Sound ANIMAL_AMIGAVEL, ANIMAL_NEUTRO, ANIMAL_RESTRITO, HOSTIL;
+    public static Sound  ANIMAL_RESTRITO, HOSTIL;
 
     // Sons por entidade quando pega emoji
-    public static Sound NINJA, MEDICO, POLICIAL, BOMBEIRO, ESTUDANTE;
+    public static Sound NINJA,  POLICIAL, ESTUDANTE;
 
     //Sons de transporte no fim da frase
-    public static Sound NINJA_TRANS, MEDICO_TRANS, POLICIAL_TRANS, BOMBEIRO_TRANS, Estudante_Trans;
+    public static Sound NINJA_TRANS, MEDICO_TRANS, POLICIAL_TRANS, BOMBEIRO_TRANS, ESTUDANTE_Trans;
 
-    //Musicas por entidades e por fase
-    public static Music MEDICO_FASE1, MEDICO_FASE2, MEDICO_FASE3;
-    public static Music POLICIAL_FASE1, POLICIAL_FASE2, POLICIAL_FASE3;
-    public static Music BOMBEIRO_FASE1, BOMBEIRO_FASE2, BOMBEIRO_FASE3;
-    public static Music ESTUDANTE_FASE1, ESTUDANTE_FASE2, ESTUDANTE_FASE3;
-    public static Music NINJA_FASE1, NINJA_FASE2, NINJA_FASE3;
+    public static Music DIA, TARDE, NOITE;
 
     public static void carregar(){
         START = Gdx.audio.newSound(Gdx.files.internal("sound/start.wav"));
         PORTA = Gdx.audio.newSound(Gdx.files.internal("sound/door.wav"));
         ERRO = Gdx.audio.newSound(Gdx.files.internal("sound/error.wav"));
-        VIDA_PERDEU= Gdx.audio.newSound(Gdx.files.internal("sound/losing.wav"));
+        VIDA_PERDIDA= Gdx.audio.newSound(Gdx.files.internal("sound/losing.wav"));
         GAMEOVER = Gdx.audio.newSound(Gdx.files.internal("sound/gameover.wav"));
         TIMER_BEEP = Gdx.audio.newSound(Gdx.files.internal("sound/countdown.wav"));
+        TIMER = Gdx.audio.newSound(Gdx.files.internal("sound/timer.wav"));
 
         //Animais
-        ANIMAL_AMIGAVEL =   Gdx.audio.newSound(Gdx.files.internal("sound/bonus.wav"));
-        ANIMAL_NEUTRO = Gdx.audio.newSound(Gdx.files.internal("sound/laser.wav"));
         ANIMAL_RESTRITO = Gdx.audio.newSound(Gdx.files.internal("sound/checkpoint.wav"));
         HOSTIL = Gdx.audio.newSound(Gdx.files.internal("sound/wolves.wav"));
 
         //Emoji por entidade
         NINJA = Gdx.audio.newSound(Gdx.files.internal("sound/bell.wav"));
-        MEDICO = Gdx.audio.newSound(Gdx.files.internal("sound/ambulance.wav"));
         POLICIAL = Gdx.audio.newSound(Gdx.files.internal("sound/police.wav"));
-        BOMBEIRO = Gdx.audio.newSound(Gdx.files.internal("sound/bell.wav"));
         ESTUDANTE = Gdx.audio.newSound(Gdx.files.internal("sound/bell.wav"));
 
         //Transporte por entidade
         NINJA_TRANS = Gdx.audio.newSound(Gdx.files.internal("sound/horse.wav"));
-        MEDICO_TRANS = Gdx.audio.newSound(Gdx.files.internal("sound/police.wav"));
         POLICIAL_TRANS = Gdx.audio.newSound(Gdx.files.internal("sound/police.wav"));
-        BOMBEIRO_TRANS = Gdx.audio.newSound(Gdx.files.internal("sound/bell.wav"));
-        Estudante_Trans = Gdx.audio.newSound(Gdx.files.internal("sound/bell.wav"));
+        ESTUDANTE_Trans = Gdx.audio.newSound(Gdx.files.internal("sound/bell.wav"));
 
-        //Musicas fase1= Dia, Fase2=Tarde, Fase3=Noite
-        MEDICO_FASE1 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic.wav"));
-        MEDICO_FASE2 = Gdx.audio.newMusic(Gdx.files.internal("sound/rain.wav"));
-        MEDICO_FASE3 = Gdx.audio.newMusic(Gdx.files.internal("sound/hospital_noite.wav"));
+        NINJA_TRANS = Gdx.audio.newSound(Gdx.files.internal("sound/horse.wav"));
+        POLICIAL_TRANS = Gdx.audio.newSound(Gdx.files.internal("sound/police.wav"));
+        ESTUDANTE_Trans = Gdx.audio.newSound(Gdx.files.internal("sound/victory.wav"));
 
-        POLICIAL_FASE1 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic.wav"));
-        POLICIAL_FASE2 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic.wav"));
-        POLICIAL_FASE3 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic.wav"));
+        DIA = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic.wav"));
+        TARDE = Gdx.audio.newMusic(Gdx.files.internal("sound/rain.wav"));
+        NOITE = Gdx.audio.newMusic(Gdx.files.internal("sound/HOSPITAL.wav"));
 
-        NINJA_FASE1 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic.wav"));
-        NINJA_FASE2 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic.wav"));
-        NINJA_FASE3 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic.wav"));
+        DIA.setLooping(true); TARDE.setLooping(true); NOITE.setLooping(true);
 
-        BOMBEIRO_FASE1 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic"));
-        BOMBEIRO_FASE2 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic"));
-        BOMBEIRO_FASE3 = Gdx.audio.newMusic(Gdx.files.internal("sound/futuristic"));
-
-        ESTUDANTE_FASE1 = Gdx.audio.newMusic(Gdx.files.internal("sout/futuristic.wav"));
-        ESTUDANTE_FASE2 = Gdx.audio.newMusic(Gdx.files.internal("sout/futuristic.wav"));
-        ESTUDANTE_FASE3 = Gdx.audio.newMusic(Gdx.files.internal("sout/futuristic.wav"));
-
-        // lopp nas musicas
-        for (Music m : new Music[]{MEDICO_FASE1, MEDICO_FASE2, MEDICO_FASE3, POLICIAL_FASE1, POLICIAL_FASE2,POLICIAL_FASE3, NINJA_FASE1, NINJA_FASE2, NINJA_FASE3, BOMBEIRO_FASE1, BOMBEIRO_FASE2, BOMBEIRO_FASE3, ESTUDANTE_FASE1, ESTUDANTE_FASE2, ESTUDANTE_FASE3}){
-            m.setLooping(true);
+        }
+        public static void dispose(){
+        START.dispose(); PORTA.dispose(); ERRO.dispose(); VIDA_PERDIDA.dispose();
+        GAMEOVER.dispose(); TIMER_BEEP.dispose(); TIMER.dispose();
+        ANIMAL_RESTRITO.dispose(); HOSTIL.dispose();
+        NINJA_TRANS.dispose(); POLICIAL_TRANS.dispose(); ESTUDANTE_Trans.dispose();
+        DIA.dispose(); TARDE.dispose(); NOITE.dispose();
         }
     }
-    public static void dispose(){
-        START.dispose(); PORTA.dispose(); ERRO.dispose(); VIDA_PERDEU.dispose();
-        GAMEOVER.dispose(); TIMER_BEEP.dispose();
-        ANIMAL_AMIGAVEL.dispose(); ANIMAL_NEUTRO.dispose(); ANIMAL_RESTRITO.dispose(); HOSTIL.dispose();
-        NINJA.dispose(); MEDICO.dispose(); POLICIAL.dispose(); BOMBEIRO.dispose(); ESTUDANTE.dispose();
-        MEDICO_FASE1.dispose(); MEDICO_FASE2.dispose(); MEDICO_FASE3.dispose();
-        POLICIAL_FASE1.dispose(); POLICIAL_FASE2.dispose(); POLICIAL_FASE3.dispose();
-        NINJA_FASE1.dispose(); NINJA_FASE2.dispose(); NINJA_FASE3.dispose();
-        BOMBEIRO_FASE1.dispose(); BOMBEIRO_FASE2.dispose(); BOMBEIRO_FASE3.dispose();
-        ESTUDANTE_FASE1.dispose(); ESTUDANTE_FASE2.dispose(); ESTUDANTE_FASE3.dispose();
-    }
-}
 
